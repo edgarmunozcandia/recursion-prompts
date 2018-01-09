cf345b0cee699e59157314cc6894ec996bfaa019
Edgar Munoz <edgarmunozcandia@gmail.com>
2018-01-09 09:40:59 -0600

    worked problems 1 through 6

--- src/recursion.js
+++ src/recursion.js
@@ -7,27 +7,63 @@
 // Example: 5! = 5 x 4 x 3 x 2 x 1 = 120
 // factorial(5); // 120
 var factorial = function(n) {
+  if(n < 0){
+    return null;
+  }
+    if(n === 0){
+    return 1;
+  }
+  return n * factorial (n - 1);
 };
+
 
 // 2. Compute the sum of an array of integers.
-// sum([1,2,3,4,5,6]); // 21
-var sum = function(array) {
-};
+  // sum([1,2,3,4,5,6]); // 21
+  var sum = function(array) {
+   if (array.length === 0){
+     return 0;
+   }
+  return array[0] + sum(array.slice(1));
+  };
 
-// 3. Sum all numbers in an array containing nested arrays.
-// arraySum([1,[2,3],[[4]],5]); // 15
-var arraySum = function(array) {
-};
+  // 3. Sum all numbers in an array containing nested arrays.
+  // arraySum([1,[2,3],[[4]],5]); // 15
+  var arraySum = function(array) {
+      if(array.length === 0) {
+        return 0;
+      } else if(Array.isArray(array[0])){
+        return arraySum(array[0]) + arraySum(array.slice(1));
+      }
+      return array[0] + arraySum(array.slice(1));
+  };
 
-// 4. Check if a number is even.
-var isEven = function(n) {
-};
+  // 4. Check if a number is even.
+  var isEven = function(n) {
+
+   if (n === 0){
+     return true;
+     }
+  if
+    (n === 1){
+     return false;
+   }
+   if (n < 0) {
+        return isEven(-n);
+      }
+
+   return isEven(n - 2);
+  };
+
 
 // 5. Sum all integers below a given integer.
 // sumBelow(10); // 45
 // sumBelow(7); // 21
 var sumBelow = function(n) {
-};
+  if(n === 0){
+    return 0;
+  }else if(n < 0){
+    return n - 1 + sumBelow(n - 1);
+  };
 
 // 6. Get the integers within a range (x, y).
 // range(2,9); // [3,4,5,6,7,8]
@@ -63,6 +99,7 @@ var palindrome = function(string) {
 // modulo(17,5) // 2
 // modulo(22,6) // 4
 var modulo = function(x, y) {
+
 };
 
 // 12. Write a function that multiplies two numbers without using the * operator or
